d7185cd4eb60c49ac43f51a1e2e3b39b10784168
hadiza <harounass2016@gmail.com>
2019-08-14 08:42:12 +0100

    ajout de la partie domaine

--- view/template.php
+++ view/template.php
@@ -21,7 +21,7 @@
                </div>
                <div class="menu">
                   <ul>
-                     <li><a href="#">Accueil</a></li>
+                     <li class="acc"><a href="#" >Accueil</a></li>  
                      <li><a href="#">Activités</a></li>
                      <li><a href="#">Produits et Services</a></li>
                      <li><a href="#">Partenaires</a></li>
@@ -37,15 +37,75 @@
                </div>
             </nav>
          </header>
-         <div class="content">
-            <p>
-               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <p>
-               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-         </div>
+<div class="content">
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+   </p>
+   <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+   </p>
+   <div class="container-fluid h-100">
+    <div class="row justify-content-center h-100">
+   <div class="col-md-8 text-center">
+      <h2>Domaine/Activités</h2>
+      <div class="card-deck">
+        <div class="card bg">
+         <a href="#"><i class="fa fa-ambulance fa-5x" aria-hidden="true"></i></a>
+          <div class="card-body">
+            <h5 class="card-title">Santé</h5>
+          </div>
+        </div>
+        <div class="card bg">
+           <a href="#"><i class="fa fa-plug fa-5x" aria-hidden="true"></i></a>
+          <div class="card-body">
+            <h5 class="card-title">Environnement</h5>
+            </div>
+        </div>
+        <div class="card bg">
+           <a href="#"><i class="fa fa-heartbeat fa-5x" aria-hidden="true"></i></a>
+          <div class="card-body">
+            <h5 class="card-title">Hygiene</h5>
+            </div>
+        </div>
       </div>
+      <br>
+      <div class="card-deck">
+        <div class="card bg">
+           <a href="#"><i class="fa fa-leaf fa-4x" aria-hidden="true"></i></a>
+          <div class="card-body">
+            <h5 class="card-title">Transformation</h5>
+            </div>
+        </div>
+        <div class="card bg">
+           <a href="#"><i class="fa fa-recycle fa-5x" aria-hidden="true"></i></a>
+          <div class="card-body">
+            <h5 class="card-title">Recyclage</h5>
+            </div>
+        </div>
+        <div class="card bg">
+           <a href="#"><i class="fa fa-life-ring fa-5x" aria-hidden="true"></i></a>
+          <div class="card-body">
+            <h5 class="card-title">Energie</h5>
+            </div>
+        </div>
+      </div>
+   </div>
+   <div class="col-md-4">
+      <div class="card-deck">
+        <div class="card bg-border">
+          <img class="card-img-top" src="public/img/femme.jpg" alt="Image directrice">
+          <div class="card-body">
+            <h5 class="card-title text-center"><b>Promotrice de Nigerielles</b></h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+     </div>
+  </div>
+</div>
+   </div>
+              
+   </div>
+</div>
 
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
