e4a5b67d98b5ddfb8972dfa074ae732d52f61dc8
Komche <Adamoukomcheabdoulrazak@gmail.com>
2019-09-03 19:35:30 +0100

    amelioration de design du site

--- view/homeView.php
+++ view/homeView.php
@@ -165,9 +165,6 @@ ob_start();
     </div>
     <div><br>
         <h1>Partenaires</h1><br>
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
         <!------ Include the above in your HEAD tag ---------->
 
         <div class="container">
@@ -239,14 +236,6 @@ ob_start();
 
     <div>
         <h2 class="text-center">GALERIE PHOTOS</h2><br>
-
-
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <!------ Include the above in your HEAD tag ---------->
-
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <div id="gallery">
             <section>
