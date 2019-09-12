a829970a38cfb55e08d36171d89f9aa1bac459c9
cherifatou <saidoumoussacherifa@gmail.com>
2019-09-12 10:50:39 +0100

    mise en place des images

--- view/homeView.php
+++ view/homeView.php
@@ -46,7 +46,7 @@ ob_start();
         <div class="col-md-4">
             <div class="card-deck">
                 <div class="card bg-border">
-                    <img class="card-img-top" src="public/img/fem.jpg" alt="Image directrice">
+                    <img class="card-img-top" src="public/img/p.jpg" alt="Image directrice">
                     <div class="card-body">
                         <h5 class="card-title text-center"><b>L'entrepreneuse du mois</b></h5>
                         <p class="card-text">Nigerielles est une association de jeunes femmes nigériennes entrepreneures et porteuses de projets qui a été créé en Mai 2017 dans le but de promouvoir l’entreprenariat et le leadership féminin au Niger...</p>
