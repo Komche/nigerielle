9e8e5d8ab0605b1f53a7bb4de74af2db675e9179
cherifatou <saidoumoussacherifa@gmail.com>
2019-09-20 16:59:17 +0100

    modifications des images

---
+++ view/actualiteView.php
@@ -0,0 +1,208 @@
+<?php 
+$title = "actualite";
+ob_start();
+?>
+    <div style="background-image: url(public/img/n1.JPG); background-repeat: no-repeat;background-size: 1500px; height: 400px"  class=" mx-auto text-center">
+                  <div  class="section-title mb-100">
+                     <h4>Nos actualités</h4>
+                  </div>
+               </div>
+    <div class="row blog">
+        <div class="col-md-12">
+            <h2 class="text-center"></h2>
+            <div id="blogCarousel" class="carousel slide container-blog" data-ride="carousel">
+                <!--  <ol class="carousel-indicators">
+                  <li data-target="#blogCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#blogCarousel" data-slide-to="1"></li>
+                </ol>
+                 Carousel items -->
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="item-box-blog">
+                                    <div class="item-box-blog-image">
+                                        <!--Date-->
+
+                                        <!--Image-->
+                                        <figure><img alt="" src="public/img/slide7.JPG"></figure>
+                                    </div>
+                                    <div class="item-box-blog-body">
+                                        <!--Heading-->
+                                        <div class="item-box-blog-heading">
+                                            <a href="#" tabindex="0">
+                                                <h5>Actualités</h5>
+                                            </a>
+                                        </div>
+                                        <!--Data-->
+                                        <div class="item-box-blog-data" style="padding: px 15px;">
+                                        </div>
+                                        <!--Text-->
+                                        <div class="item-box-blog-text">
+                                            <p>Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, consectetuer adipiscing. Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, consectetuer adipiscing. Lorem ipsum dolor.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="item-box-blog">
+                                    <div class="item-box-blog-image">
+                                        <!--Date-->
+                                        <!--Image-->
+                                        <figure> <img alt="" src="public/img/actu2.JPG"> </figure>
+                                    </div>
+                                    <div class="item-box-blog-body">
+                                        <!--Heading-->
+                                        <div class="item-box-blog-heading">
+                                            <a href="#" tabindex="0">
+                                                <h5>Actualités</h5>
+                                            </a>
+                                        </div>
+                                        <!--Data-->
+                                        <div class="item-box-blog-data" style="padding: px 15px;">
+                                        </div>
+                                        <!--Text-->
+                                        <div class="item-box-blog-text">
+                                            <p>Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, consectetuer adipiscing. Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, consectetuer adipiscing. Lorem ipsum dolor.</p>
+                                        </div>
+                                        <!--Read More Button-->
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="item-box-blog">
+                                    <div class="item-box-blog-image">
+                                        <!--Date-->
+                                        <!--Image-->
+                                        <figure> <img alt="" src="public/img/actu3.JPG"> </figure>
+                                    </div>
+                                    <div class="item-box-blog-body">
+                                        <!--Heading-->
+                                        <div class="item-box-blog-heading">
+                                            <a href="#" tabindex="0">
+                                                <h5>Actualités</h5>
+                                            </a>
+                                        </div>
+                                        <!--Data-->
+                                        <div class="item-box-blog-data" style="padding: px 15px;">
+
+                                        </div>
+                                        <!--Text-->
+                                        <div class="item-box-blog-text">
+                                            <p>Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, consectetuer adipiscing. Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, consectetuer adipiscing. Lorem ipsum dolor.</p>
+                                        </div>
+
+                                        <!--Read More Button-->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--.row-->
+                        
+
+                    </div>
+                </div>
+            </div>
+            <div class="row blog">
+        <div class="col-md-12">
+            <h2 class="text-center"></h2>
+            <div id="blogCarousel" class="carousel slide container-blog" data-ride="carousel">
+                <!--  <ol class="carousel-indicators">
+                  <li data-target="#blogCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#blogCarousel" data-slide-to="1"></li>
+                </ol>
+                 Carousel items -->
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="item-box-blog">
+                                    <div class="item-box-blog-image">
+                                        <!--Date-->
+
+                                        <!--Image-->
+                                        <figure><img alt="" src="public/img/slide1.JPG"></figure>
+                                    </div>
+                                    <div class="item-box-blog-body">
+                                        <!--Heading-->
+                                        <div class="item-box-blog-heading">
+                                            <a href="#" tabindex="0">
+                                                <h5>Actualités</h5>
+                                            </a>
+                                        </div>
+                                        <!--Data-->
+                                        <div class="item-box-blog-data" style="padding: px 15px;">
+                                        </div>
+                                        <!--Text-->
+                                        <div class="item-box-blog-text">
+                                            <p>Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, consectetuer adipiscing. Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, consectetuer adipiscing. Lorem ipsum dolor.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="item-box-blog">
+                                    <div class="item-box-blog-image">
+                                        <!--Date-->
+                                        <!--Image-->
+                                        <figure> <img alt="" src="public/img/slide2.JPG"> </figure>
+                                    </div>
+                                    <div class="item-box-blog-body">
+                                        <!--Heading-->
+                                        <div class="item-box-blog-heading">
+                                            <a href="#" tabindex="0">
+                                                <h5>Actualités</h5>
+                                            </a>
+                                        </div>
+                                        <!--Data-->
+                                        <div class="item-box-blog-data" style="padding: px 15px;">
+                                        </div>
+                                        <!--Text-->
+                                        <div class="item-box-blog-text">
+                                            <p>Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, consectetuer adipiscing. Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, consectetuer adipiscing. Lorem ipsum dolor.</p>
+                                        </div>
+                                        <!--Read More Button-->
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="item-box-blog">
+                                    <div class="item-box-blog-image">
+                                        <!--Date-->
+                                        <!--Image-->
+                                        <figure> <img alt="" src="public/img/slide5.JPG"> </figure>
+                                    </div>
+                                    <div class="item-box-blog-body">
+                                        <!--Heading-->
+                                        <div class="item-box-blog-heading">
+                                            <a href="#" tabindex="0">
+                                                <h5>Actualités</h5>
+                                            </a>
+                                        </div>
+                                        <!--Data-->
+                                        <div class="item-box-blog-data" style="padding: px 15px;">
+
+                                        </div>
+                                        <!--Text-->
+                                        <div class="item-box-blog-text">
+                                            <p>Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, consectetuer adipiscing. Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, consectetuer adipiscing. Lorem ipsum dolor.</p>
+                                        </div>
+
+                                        <!--Read More Button-->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--.row-->
+                        
+
+                    </div>
+                </div>
+            </div>
+            <button type="button" name="button" class="btn login_btn voir-plus">Plus d'actualités</button>
+        </div>
+    </div>
+    <?php
+$content = ob_get_clean();
+require('template.php');
+?>
