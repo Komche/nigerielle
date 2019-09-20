3634c64cd514336d9b89359783eb22f45d1acf28
cherifatou <saidoumoussacherifa@gmail.com>
2019-09-20 14:17:26 +0100

    creation du formulaire

--- view/contactView.php
+++ view/contactView.php
@@ -2,55 +2,59 @@
 $title = "Contact";
 ob_start();
 ?>
-    <section class="contact pt-100 pb-100" id="contact">
-         <div class="container">
-            <div class="row">
-               <div class="col-xl-6 mx-auto text-center">
-                  <div class="section-title mb-100">
-                     <p>get in touch</p>
-                     <h4>contact me</h4>
+    <section style="padding-top: 0;" class="contact pt-100 pb-100" id="contact">
+    <div style="background-image: url(public/img/n3.JPG); height: 400px" class=" mx-auto text-center">
+                  <div  class="section-title mb-100">
+                     <h4>Contactez-nous</h4>
                   </div>
                </div>
+         <div class="container">
+            <div class="row">
             </div>
             <div class="row text-center">
                   <div class="col-md-8">
                      <form action="#" class="contact-form">
                         <div class="row">
                            <div class="col-xl-6">
-                              <input type="text" placeholder="name">
+                              <input type="text" placeholder="Nom">
+                           </div>
+                           <div class="col-xl-6">
+                              <input type="text" placeholder="Prenom">
                            </div>
                            <div class="col-xl-6">
                               <input type="text" placeholder="email">
                            </div>
-                           <div class="col-xl-6">
-                              <input type="text" placeholder="subject">
-                           </div>
+
                            <div class="col-xl-6">
                               <input type="text" placeholder="telephone">
                            </div>
+                           <div class="col-xl-6">
+                              <input type="text" placeholder="Objet">
+                           </div>
                            <div class="col-xl-12">
                               <textarea placeholder="message" cols="30" rows="10"></textarea>
-                              <input type="submit" value="send message">
+                              <input type="submit" value="Envoyer message">
                            </div>
                         </div>
                      </form>
-                  </div>
+                     </div>
                   <div class="col-md-4">
                      <div class="single-contact">
                         <i class="fa fa-map-marker"></i>
-                        <h5>Address</h5>
-                        <p>661 Lefferts, NY 11203, USA</p>
+                        <h5>Adresse</h5>
+                        <p>Niamey-Niger</p>
                      </div>
                      <div class="single-contact">
                         <i class="fa fa-phone"></i>
-                        <h5>Phone</h5>
-                        <p>(+1) 517 397 7100</p>
+                        <h5>Telephone</h5>
+                        <p>93906364/90956364</p>
                      </div>
                      <div class="single-contact">
                         <i class="fa fa-envelope"></i>
                         <h5>Email</h5>
-                        <p>infor@personal.com</p>
+                        <p>contact.nigerielles@.ne</p>
                      </div>
+                  </div>
                   </div>
             </div>
          </div>
